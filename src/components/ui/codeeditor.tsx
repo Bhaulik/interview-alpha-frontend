fd05031def682d4a444cf8dac90a295dc042d076
Bhaulik Patel <bhaulikpatel966@gmail.com>
2024-04-26 23:42:14 -0500

    setup - wip for code o practice

---
+++ src/components/ui/codeeditor.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+import { useCodeMirror } from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import CodeMirror from "@uiw/react-codemirror";
+
+function CodeEditor({ code = "console.log('Code Mirror!!!');" }) {
+  return <CodeMirror value={code} height="500px" />;
+}
+
+export default CodeEditor;
